b2b638f79e3dcf3df2d937bba890f8a35b88e2f9
Jayden Pyles <jpylesbusiness@gmail.com>
2024-04-24 20:12:23 -0500

    wip: add full preview

--- src/components/Applicant/FullJobPreview.js
+++ src/components/Applicant/FullJobPreview.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Typography from "@mui/material/Typography"; // Ensure Typography is correctly imported from MUI
+import { Button } from "@mui/base";
 
 const FullJobPreview = ({ job }) => {
   return (
@@ -9,7 +10,14 @@ const FullJobPreview = ({ job }) => {
           id="full-job-preview"
           className="bg-slate-600 rounded-md max-h-screen overflow-scroll p-2 text-white w-full"
         >
-          <Typography variant="h5">{job.job_title}</Typography>
+          <div className="flex flex-row">
+            <Typography variant="h5" className="w-3/4">
+              {job.job_title}
+            </Typography>
+            <Button className="m-auto bg-blue-500 p-2 rounded-md">
+              Easy Apply
+            </Button>
+          </div>
           <Typography>{job.company}</Typography>
           <Typography>{job.job_type}</Typography>
           <Typography>{job.salary}</Typography>
