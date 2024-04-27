8cc80e66dafa60bfeb35f6ce3fc7a8f40f6d4e28
AJ Nettles <ajnettles@gmail.com>
2024-04-26 19:49:10 -0500

    job applying and statuses

--- src/components/Applicant/FullJobPreview.js
+++ src/components/Applicant/FullJobPreview.js
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography"; // Ensure Typography is correctly imported from MUI
 import { Button } from "@mui/base";
+import { useAuth0 } from "@auth0/auth0-react";
+import { API_URL } from "../../lib/Constants";
+
 
 const FullJobPreview = ({ job }) => {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+  const [applied, setApplied] = useState(null);
+
+  const handleApply = async (e) => {
+    e.preventDefault();
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await fetch(`${API_URL}/apply`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(job),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to apply for position");
+      }
+
+      console.log("Application sent!");
+      alert("Application sent!");
+      setApplied("Applied");
+
+    } catch (error) {
+      console.error("Error sending application:", error);
+    }
+  };
+
+  const fetchJobStatus = async () => {
+    if (!isAuthenticated) return;
+    try {
+      setApplied("Loading")
+      const token = await getAccessTokenSilently();
+      const response = await fetch(`${API_URL}/job_status`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(job),
+      });
+      
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      
+      const data = await response.json();
+      setApplied(data.status);
+    } catch (error) {
+      console.error("Error fetching job status:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchJobStatus()
+  }, [job]);
   return (
     <div className="mt-4 mb-4 w-1/2 mr-4">
       {job ? (
@@ -14,9 +74,15 @@ const FullJobPreview = ({ job }) => {
             <Typography variant="h5" className="w-3/4">
               {job.job_title}
             </Typography>
-            <Button className="m-auto bg-blue-500 p-2 rounded-md">
-              Easy Apply
-            </Button>
+            {applied ? 
+              <Button className="m-auto bg-green-500 p-2 rounded-md">
+                {applied}
+              </Button>
+              :
+              <Button className="m-auto bg-blue-500 p-2 rounded-md" onClick={handleApply}>
+                Easy Apply
+              </Button>
+            }
           </div>
           <Typography>{job.company}</Typography>
           <Typography>{job.job_type}</Typography>
--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -4,18 +4,30 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const { logout } = useAuth0();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     logout({ returnTo: window.location.origin });
+  };
+
+  const handleRedirect = (redirectLink) => {
+    navigate(redirectLink);
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
         <Toolbar>
+          <Button color="inherit" onClick={() => handleRedirect("/jobs")}>
+            Jobs
+          </Button>
+          <Button color="inherit" onClick={() => handleRedirect("/applicant")}>
+            Profile
+          </Button>
           <Button color="inherit" onClick={handleLogout}>
             Logout
           </Button>
