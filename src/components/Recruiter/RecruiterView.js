70248d1e75cb1c5bea35df8b2cf6cd4c6c6e450f
Jayden Pyles <jpylesbusiness@gmail.com>
2024-04-28 14:05:34 -0500

    feat: view applications for a recruiter

--- src/components/Recruiter/RecruiterView.js
+++ src/components/Recruiter/RecruiterView.js
@@ -1,9 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { API_URL } from "../../lib/Constants";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@mui/material";
 
 const RecruiterView = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await fetch(`${API_URL}/recruiter_jobs`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setJobs(data.jobs);
+    } catch (error) {
+      console.error("Error fetching profile data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-full max-w-md mt-4"></div>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center">
+      <div id="table" className="h-full">
+        <TableContainer component={Paper} className="px-8 pt-6 pb-8 mb-4 mt-4">
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Applicant</TableCell>
+                <TableCell>Applicant Email</TableCell>
+                <TableCell>Time Applied</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {jobs &&
+                jobs.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{row.jobTitle}</TableCell>
+                    <TableCell>{row.status}</TableCell>
+                    <TableCell>{row.fullName}</TableCell>
+                    <TableCell>{row.applicantEmail}</TableCell>
+                    <TableCell>{row.timeCreated}</TableCell>
+                    <TableCell>
+                      <Button size="small">View Application</Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 };
